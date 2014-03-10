209422047807df3d04819a472742b8ca67b189db
Dimitris Asvestis <dimitris.asvestis@gmail.com>
2014-03-10 16:55:03 +0100

    first

--- audit-vaadin-ejb/src/test/java/eu/ec/inea/ejb/providers/UserEntityProviderTest.java
+++ audit-vaadin-ejb/src/test/java/eu/ec/inea/ejb/providers/UserEntityProviderTest.java
@@ -1,7 +1,36 @@
 package eu.ec.inea.ejb.providers;
 
+import eu.ec.inea.domain.User;
+import eu.ec.inea.ejb.providers.impl.UserEntityProvider;
+import junit.framework.TestCase;
+
+import javax.ejb.embeddable.EJBContainer;
+import javax.naming.Context;
+import java.util.HashMap;
+import java.util.Properties;
+
+
 /**
  * Created by asvesdi on 10/03/14.
  */
-public class UserEntityProviderTest {
+public class UserEntityProviderTest extends TestCase{
+
+    public void test() throws Exception {
+        final Properties p = new Properties();
+//        p.setProperty("movieDatabase", "new://Resource?type=DataSource");
+//        p.setProperty("movieDatabase.JdbcDriver", "org.hsqldb.jdbcDriver");
+//        p.setProperty("movieDatabase.JdbcUrl", "jdbc:hsqldb:mem:moviedb");
+
+
+        final Context context = EJBContainer.createEJBContainer(p).getContext();
+        UserEntityProvider provider = (UserEntityProvider) context.lookup("java:global/audit-vaadin-ejb/UserEntityProvider");
+
+        User u = new User();
+        u.setFirstName("Dimitris");
+        u.setLastName("Asvestis");
+
+
+    }
+
+
 }
